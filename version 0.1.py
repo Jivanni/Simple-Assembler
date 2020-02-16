437e2be5cc54bc86abd985efa0397d31e2e392fe
Jivanni <cusumano.giovanni94@gmail.com>
2020-02-16 21:57:26 +0000

    version0.2

--- version 0.1.py
+++ version 0.1.py
@@ -1,24 +1,42 @@
 def simple_assembler(program):
     registers = {}
-    array = []
-    for line in program:
-        array.append(line.split(" "))
-    pointer = 0
-    while pointer < len(array):
     
-        if array[pointer][0] == "mov" and array[pointer][2].isnumeric():
-            registers[array[pointer][1]] = int(array[pointer][2])
-            pointer+=1
-        elif array[pointer][0] == "mov" and array[pointer][2].isalpha():
-            registers[array[pointer][1]] = registers[array[pointer][2]]
+    def parser(program):
+        array = []
+        for line in program:
+            array.append(line.split(" "))
+        for instruction in array:
+            if instruction[1].isdigit():
+                instruction[1] = int(instruction[1])
+            if len(instruction) > 2 :
+                instruction[2] = int(instruction[2])
+        return array
+    array = parser(program)
+    pointer = 0
+    
+    while pointer < len(array):
+        if array[pointer][0] == "mov":
+            if isinstance(array[pointer][2], int):
+                registers[array[pointer][1]] = array[pointer][2]
+            else: registers[array[pointer][1]] = registers[array[pointer][2]]
+            print(f"instruction{array[pointer]}executed correctly with pointer value of {pointer}")
             pointer+=1
         elif array[pointer][0] == "inc":
             registers[array[pointer][1]]+=1
+            print(f"instruction{array[pointer]}executed correctly with pointer value of {pointer}")
             pointer+=1
         elif array[pointer][0]=="dec":
             registers[array[pointer][1]]-=1
+            print(f"instruction{array[pointer]}executed correctly with pointer value of {pointer}")
             pointer+=1
-        elif array[pointer][0]=="jnz" and registers[array[pointer][1]] !=0:
-            pointer+=int(array[pointer][2])
+        elif array[pointer][0]=="jnz":
+            if array[pointer][1] in registers and registers[array[pointer][1]]!= 0 and isinstance(array[pointer][1],int) and array[pointer][1]!=0:
+                print(f"instruction{array[pointer]}executed correctly with pointer value of {pointer}")
+                pointer+=int(array[pointer][2])
+
+            else: pointer+=1
         else: pointer+=1
-    return registers
+    return print(registers)
+    
+
+simple_assembler(['mov a 5','inc a', 'mov b 56','jnz 0 2', 'dec a','dec a','jnz a -1','inc a'])
